af9f58866c5bd91b264c9d6157c728522c792bca
Ferdiansyah Annora <ferdianrra@student.gunadarma.ac.id>
2026-09-20 23:37:37 +0700

    Initial Commit

---
+++ capfightTests/capfightTests.swift
@@ -0,0 +1,19 @@
+//
+//  capfightTests.swift
+//  capfightTests
+//
+//  Created by Ferdiansyah Annora on 20/09/26.
+//
+
+import Testing
+@testable import capfight
+
+struct capfightTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
